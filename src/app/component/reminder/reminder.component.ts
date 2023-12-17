722a5666a4eb3e7337cdf5f1ae5bcdf624f903ed
mrxpranav <mrxpranav@gmail.com>
2023-12-17 14:06:38 +0530

    added reminder

--- src/app/component/reminder/reminder.component.ts
+++ src/app/component/reminder/reminder.component.ts
@@ -11,7 +11,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class ReminderComponent {
   Reminders: Reminder[];
   rem: Reminder = new Reminder;
-  
+  updateRemId: any = 0;
 
   constructor(private remServ: ReminderService){
 
@@ -52,6 +52,39 @@ saveReminder(){
   this.reminderForm.reset();
   this.getProducts();
 
+}
+
+editReminder(reminder : Reminder){
+  this.updateRemId = reminder.id;
+  console.log("Editing reminder :"+this.updateRemId);
+  this.reminderForm.controls.Name.setValue(reminder.name);
+  this.reminderForm.controls.title.setValue(reminder.title);
+  this.reminderForm.controls.cat.setValue(reminder.category);
+}
+
+updateReminder(){
+  if (this.updateRemId != 0) {
+  console.log("Updating reminder :"+this.updateRemId);
+  this.rem = new Reminder();
+  this.rem.name = this.reminderForm.get("Name")?.value;
+  this.rem.title = this.reminderForm.get("title")?.value;
+  this.rem.category = this.reminderForm.get("cat")?.value;
+  this.rem.enable = "true";
+  this.remServ.saveReminder(this.rem).subscribe();
+  this.reminderForm.reset();
+  this.getProducts();
+  this.updateRemId = 0;
+  } else {
+    console.log("Unable to update reminder with null id");
+  }
+  
+
+}
+
+deleteReminder(id :any){
+  console.log("Deleting reminder :"+id);
+  this.remServ.delReminder(id).subscribe();
+  this.getProducts();
 }
 
 }
